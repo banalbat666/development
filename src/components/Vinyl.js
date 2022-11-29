1f73171c5c3868f2bd8e086869c5cd9cb1d4e0cb
Elizabeth <elizabeth_c_wu@brown.edu>
2022-11-29 03:26:20 -0500

    no more add

--- src/components/Vinyl.js
+++ src/components/Vinyl.js
@@ -1,15 +1,6 @@
 import './../App.css';
-// import AddButton from "./AddButton.js";
-import { useState } from 'react';
-
 
 export default function Vinyl(props) {
-    // const [btnText, setBtnText] = useState("Add to Cart");
-    // const [btnState, setBtnState] = useState(true);
-    // const [m, setM] = useState(new Map());
-    // console.log(m);
-
-    // console.log(props.cart);
     return (
         <div className="VinylItem">
             <img src={props.cover} alt={props.name + " by " + props.artist} id="img"/>
@@ -19,7 +10,6 @@ export default function Vinyl(props) {
                 <p>Decade: {props.decade}</p>
                 <p>Price: ${props.price}</p>
             </div>
-            {/* <AddButton totalPrice={props.totalPrice} setTotalPrice={props.setTotalPrice} itemPrice={props.price} itemName={props.name} cart={props.cart} setCart={props.setCart} /> */}
             <button
                 onClick={() => 
                     {
@@ -28,17 +18,11 @@ export default function Vinyl(props) {
                             props.setTotalPrice(Math.round((props.totalPrice + props.price) * 1e12) / 1e12);
                             // add item to cart
                             props.setCart([...props.cart, props.name]);
-                            // setBtnText("Remove from Cart");
-                            // props.setBtnMap(props.btnMap.set(props.name, false));
-                            // setBtnState(false);
                         } else { // click remove from cart
                             // fixes js float math
                             props.setTotalPrice(Math.round((props.totalPrice - props.price) * 1e12) / 1e12);
                             // remove item from cart
                             props.setCart(props.cart.filter((item) => item !== props.name));
-                            // setBtnText("Add to Cart");
-                            // props.setBtnMap(props.btnMap.set(props.name, true));
-                            // setBtnState(true);
                         }
                     }}
                 >
