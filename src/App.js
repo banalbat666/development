5a4c47f5acf09d95f70ec7c9eadf6bd9901cf9da
Elizabeth <elizabeth_c_wu@brown.edu>
2022-11-28 21:21:55 -0500

    comments

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ vinylData.forEach((item) => {
 	item.image = process.env.PUBLIC_URL + "/" + item.image;
 });
 
+// prints cart items, returns array of <li> elements
 function PrintCart(arr) {
 	let ret = [];
 
@@ -36,15 +37,20 @@ function App() {
 		}
 	];
 
-	function sortRecords(type) {
-		setSorter(type);
-		setInfo((prevInfo) => [...prevInfo].sort((itemA, itemB) => {return itemA[type] - itemB[type]}));
+	function sortRecords(sortType) {
+		setSorter(sortType);
+		// sort numerical values low to high
+		setInfo((prevInfo) => [...prevInfo].sort(
+			(itemA, itemB) => {return itemA[sortType] - itemB[sortType]}));
 	}
 
+	// same as sortRecords, except sorts vinylData directly (for filtering purposes)
+	// instead of 'info', the copy of vinylData
 	function jankSort(type) {
 		vinylData.sort((itemA, itemB) => itemA[type] - itemB[type]);
 	}
 
+	// returns list of records where applied filters hold true
 	function applyFilters(filters) {
 		if (filters.length === 0) {
 			jankSort(sorter);
@@ -55,12 +61,16 @@ function App() {
 		jankSort(sorter);
 		vinylData.forEach((record) => {
 			let applies = true;
+
 			filters.forEach((f) => {
+				// conditional ensures that all selected filters apply to a given record
+				// if one filter is false, 'applies' can never be set to true again
 				if (record[f.filterType] !== f.option) {
 					applies = false;
 				}
 			});
-			if (applies) {
+			
+			if (applies) { // all selected filters apply
 				ret.push(record);
 			}
 		})
@@ -71,18 +81,16 @@ function App() {
 	function filterRecords(option, filterType) {
 		let ret = [];
 
-		if (appliedFilters.some((f) => f.option === option)) {
-			ret = appliedFilters.filter((elt) => elt.option !== option);
-		} else {
+		// the user hasn't applied the filter yet => apply the filter
+		if (!appliedFilters.some((f) => f.option === option)) {
 			ret = [...appliedFilters, {option, filterType}];
+		} else { // user unchecks a filter checkox => no longer apply the filter
+			ret = appliedFilters.filter((elt) => elt.option !== option);
 		}
 
 		setAppliedFilters(ret);
+		// set records to display to be the filtered list of records
 		setInfo(applyFilters(ret));
-	}
-
-	function displayFilters(group) {
-		return <Filter filterGroup={group} filterType={group.type} filterRecords={filterRecords} />;
 	}
 
 	return (
@@ -98,7 +106,9 @@ function App() {
 
 				<div className="Side-Subsect">
 					<h2 id="light-text"><u>Filter By:</u></h2>
-					{filterGroups.map(displayFilters)}
+					{filterGroups.map((group) => (
+						<Filter filterGroup={group} filterType={group.type} filterRecords={filterRecords} />
+					))}
 				</div>
 
 				<div className="Side-Subsect">
